694c4094568bd8c91db881d4a54cee6a53dab9ab
Rümeysa <70983764+rumeysaguc@users.noreply.github.com>
2025-12-05 03:33:45 +0300

    Update tasks.py

--- section-2/tasks.py
+++ section-2/tasks.py
@@ -4,20 +4,15 @@ from requests.exceptions import RequestException
 
 @shared_task(bind=True, max_retries=5)
 def notify_insurance_company_task(self, request_id):
-    """
-    Sigorta şirketine bildirim yapan task
-    - Hata olursa retry eder (exponential backoff)
-    """
+  
     url = f"https://mock-insurance-service.com/notify/{request_id}"
 
     try:
         response = requests.post(url, timeout=5)  # 5 saniye timeout
-        response.raise_for_status()  # HTTP error varsa exception fırlatır
+        response.raise_for_status()  # HTTP error varsa exception at
 
     except RequestException as exc:
-        # Exponential backoff: 2^retry * 10 saniye
-        countdown = 2 ** self.request.retries * 10
+        countdown = 2 ** self.request.retries * 10 #Bu da case study de belirtilen üstel artışla geri dönüş süresi
         raise self.retry(exc=exc, countdown=countdown)
 
     return {"status": "success", "request_id": request_id}
-
